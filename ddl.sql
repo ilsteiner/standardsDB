f35ba0dcb96a02549312add01767a425efea314a
BearThatCodes <izakizak@gmail.com>
2014-11-18 15:46:37 -0500

    Look ma! Completed DDL!

--- ddl.sql
+++ ddl.sql
@@ -28,19 +28,106 @@ spool standardsDB_DDL.sql
 
 create table tbElement (
 	symbol char(2) primary key,
-	number char(3) not null,
+	atomicNumber char(3) not null,
 	name varchar2(13) not null,
 	density number(4,2) not null
 	);
 
 create table tbType (
-	--TODO: Can this be a smallint? Does Oracle support that?
-	typeID integer primary key,
+	typeID char(2) primary key,
 	typeName varchar2(16) not null
 	);
 
 create table tbCertStatus (
-	statusID integer primary key,
+	statusID char(2) primary key,
 	statusDesc varchar2(16) not null
 	);
 
+create table tbTechnician (
+	technicianID char(2) primary key,
+	name varchar2(512) not null,
+	title varchar2(256)
+	);
+
+create table tbStandardType (
+	typeID char(2) primary key,
+	standardType varchar2(64) not null
+	);
+
+create table tbCertification (
+	certNumber char(11) primary key,
+	technicianID char(2) not null,
+	statusID char(2) not null,
+	constraint format_certNumber
+	check (REGEXP_LIKE(certNumber,'C[0-9]{10}')),
+	constraint fk_technician_tbCertification
+	foreign key (technicianID)
+	references tbTechnician(technicianID),
+	constraint fk_status_tbCertification
+	foreign key (statusID)
+	references tbCertStatus(statusID)
+	);
+
+create table tbProduct (
+	partNumber char(11) primary key,
+	typeID char(2),
+	targetValue number(5,0) not null,
+	price number(8,2),
+	stock number(5,0) default 0 not null,
+	custom char(1) not null,
+	constraint format_partNumber_tbProduct
+	check (REGEXP_LIKE(partNumber,'P[0-9]{10}')),
+	constraint bool_custom_tbProduct
+	check (custom in('Y','N')),
+	constraint fk_typeID_tbProduct
+	foreign key (typeID)
+	references tbStandardType(typeID)
+	);
+
+create table tbStandard (
+	serialNumber char(11) primary key,
+	partNumber char(11) not null,
+	certNumber char(11),
+	actualValue number(8,2),
+	constraint format_serialNumber_tbStandard
+	check (REGEXP_LIKE(serialNumber,'S[0-9]{10}')),
+	constraint fk_partNumber_tbStandard
+	foreign key (partNumber)
+	references tbProduct(partNumber),
+	constraint fk_certNumber_tbStandard
+	foreign key (certNumber)
+	references tbCertification(certNumber)
+	);
+
+create table tbProductComponent (
+	partNumber char(11),
+	symbol char(2),
+	composition number(2,0),
+	constraint format_composition_tbProductCo
+	check (composition between 1 and 100),
+	constraint fk_partNumber_tbProductCompone
+	foreign key (partNumber)
+	references tbProduct(partNumber),
+	constraint fk_symbol_tbProductComponent
+	foreign key (symbol)
+	references tbElement(symbol),
+	constraint pk_tbProductComponent
+	primary key (partNumber,symbol)
+	);
+
+create table tbStandardComponent (
+	serialNumber char(11),
+	symbol char(2),
+	partNumber char(11) not null,
+	composition number(2,0) not null,
+	constraint format_composition
+	check (composition between 1 and 100),
+	constraint fk_serialNumber_tbStandardComp
+	foreign key (serialNumber)
+	references tbStandard(serialNumber),
+	constraint fk_pComponent_tbStandardCompon
+	foreign key (symbol,partNumber)
+	references tbProductComponent(symbol,partNumber),
+	constraint pk_tbStandardComponent
+	primary key (serialNumber,symbol)
+	);
