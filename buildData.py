41c9a5554a05d8263c59b3a1de7e4c8c1b366e8f
BearThatCodes <izakizak@gmail.com>
2014-11-20 15:45:33 -0500

    Added some product inserts to the DDL file.

--- buildData.py
+++ buildData.py
@@ -16,16 +16,11 @@ with open("elements.json") as theFile:
 		theData["elements"][i]["thicknesses"] = [2,4,10,20,40,80,120,160,200,240,280,320,360,400,440,480,520,560,600,640,680,720,760,800]
 		price = random.randint(500,1000)
 		for j in range(0,len(theData["elements"][i]["thicknesses"])):
-			output = ",".join([theData["elements"][i]["element1"],theData["elements"][i]["element2"],str(theData["elements"][i]["thicknesses"][j]),str(random.randint(5,500))])
-			# print("Element 1: " + theData["elements"][i]["element1"])
-			# print("Element 2: " + theData["elements"][i]["element2"])
-			# print("Thickness: " + str(theData["elements"][i]["thicknesses"][j]))
+			output = ",".join([theData["elements"][i]["element1"],theData["elements"][i]["element2"],theData["elements"][i]["plated"],str(theData["elements"][i]["thicknesses"][j]),str(random.randint(5,200))])
 			if(theData["elements"][i]["element2"] == ""):
-				# print("Price: " + str(custround(price)))
 				output = ",".join([output,str(custround(price))])
 			else:
 				output = ",".join([output,str(custround(price*1.3))])
-				# print("Price: " + str(custround(price*1.3)))
 
 			print(output)
 
