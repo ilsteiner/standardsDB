cafa4d2414d86c11a170abf5500419ba766bdc00
BearThatCodes <izakizak@gmail.com>
2014-11-21 17:04:05 -0500

    Fixed the script and made it so that things print prettier. Also it now prints certNumbers.

--- makeStandards.py
+++ makeStandards.py
@@ -7,6 +7,10 @@ partFile = open("parts.json")
 elems = json.load(elemFile)
 parts = json.load(partFile)
 standards = []
+numCerts = 1
+numStandards = 0
+currCert = 1111111111
+certifications = [currCert]
 
 class Standard(object):
 	partNumber = ""
@@ -19,7 +23,7 @@ class Standard(object):
 		self.actualValue = actualValue
 
 	def __repr__(self):
-		return self.partNumber + "," + self.certNumber + "," + str(self.actualValue) + '\n'
+		return self.partNumber + "," + self.certNumber + "," + str(self.actualValue)
 
 def make_standard(partNumber,certNumber,actualValue):
 	newStandard = Standard(partNumber,certNumber,actualValue)
@@ -30,8 +34,22 @@ def percentDiff(value,percent):
 
 for p in range (0,len(parts["parts"])):
 	for i in range (0,parts["parts"][p]["Stock"]):
-		# actualValue = round(random.uniform(percentDiff(parts["parts"][p]["Thickness"],-6),percentDiff(parts["parts"][p]["Thickness"],6)),2)
+		# Set the actualValue to a random number from a normal distribuition whose standard deviation is 5% 
+		# Roughly 68% of standards will be within the error tolerance
 		actualValue = round(random.normalvariate(parts["parts"][p]["Thickness"],.05),2)
-		standards.append(make_standard(parts["parts"][p]["partNumber"],"",actualValue))
+		standards.append(make_standard(parts["parts"][p]["partNumber"],'C'+str(currCert),actualValue))
+		numStandards += 1
 
-print(standards)
+		# We'll probably increment the cert number, but we might not
+		if(random.random() < .93):
+			currCert += 1
+			numCerts += 1
+			certifications.append(currCert)
+print('We created ' + str(numStandards) + ' standards.')
+print('We created ' + str(numCerts) + ' corresponding certifications.')
+print('*' * 100)
+for num in certifications:
+	print(num)
+print('*' * 100)
+for s in standards:
+	print(s)
